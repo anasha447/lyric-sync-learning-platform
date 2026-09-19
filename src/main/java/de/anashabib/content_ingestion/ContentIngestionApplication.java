0a78a92da8f3454df26a9f2d6717ddd857a2fa79
anasha447 <anashabib0101@gmail.com>
2026-09-19 21:48:26 +0300

    initialise content-ingestion service

---
+++ src/main/java/de/anashabib/content_ingestion/ContentIngestionApplication.java
@@ -0,0 +1,12 @@
+package de.anashabib.content_ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentIngestionApplication.class, args);
+	}
+}
---
+++ src/test/java/de/anashabib/content_ingestion/ContentIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.anashabib.content_ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
