4bb3e494b299079fc9a0bffd93aa64c5bcefd51e
anasha447 <anashabib0101@gmail.com>
2026-09-23 00:53:03 +0300

    fix(test): add testcontainers config to main application test

--- src/test/java/de/anashabib/content_ingestion/ContentIngestionApplicationTests.java
+++ src/test/java/de/anashabib/content_ingestion/ContentIngestionApplicationTests.java
@@ -2,12 +2,20 @@ package de.anashabib.content_ingestion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class ContentIngestionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
 
+    @Test
+    void contextLoads() {
+    }
 }
